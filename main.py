fdb4c911e948d70c4a39b0e967a9b0d291ae4b96
LucasSchweisthal2024 <818482@mystma.org>
2021-10-05 14:43:32 -0500

    sfsdfsdffsfdsdf

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #WRITE YOUR CODE IN THIS FILE
+def hasL(w):
+    for i in range(0, len(w)):
+        if i == "l":
+            return True
+        else:
+            return False
+
+print(hasL("pee"))
